36642d9b93920968af8cbbfe888c198b8ffe4e48
Pavich Komansil <phavit_k@hotmail.com>
2021-12-09 16:38:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = 'YOUR_TOKEN';
+
+bot.on('message' , msg=>{
+    if(msg.content ===  "!verify"){
+        const embed = new Discord.MessageEmbed()
+            .setTitle("✅ **Verified!**")
+            .setDescription("You have been verified!")
+        let role = msg.guild.roles.cache.get("YOUR ROLES ID");
+        msg.member.roles.add(role);
+        msg.react("✅")
+        msg.author.send(embed)
+        console.log('New member verified!');
+        msg.delete({timeout:"2000"})
+    }
+})
+bot.login(token);
